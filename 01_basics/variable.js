b36b77cb9b121c810f8e0f8d3bd6f5bb656b1c2a
Ritesh Fauzdar <95282204+rjfzdr@users.noreply.github.com>
2024-01-12 16:53:47 +0000

    variable and configure done

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+
+//  variable are used to store the data in memory
+
+const accountId = 123456;
+let accountName = "Ritesh Fauzdar";
+var accountPassword = "1234"
+
+// accountId = 1778; can't change the const variable value
+
+accountName = "Akash kuntal";
+accountPassword = "21789634";
+
+console.log(accountId);
+console.table([accountId,accountName,accountPassword]);
